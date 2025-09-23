db0f5f384078496fd3d43b1bfab45837531d1225
Alexander Drew <alexander.william.drew@gmail.com>
2025-09-23 13:31:39 +0100

    Added logistic regression into function and imported necessary libraries for tkinter

--- Self_serve_ABtest_tool.py
+++ Self_serve_ABtest_tool.py
@@ -1,35 +1,38 @@
 import pandas as pd
 import numpy as np
 import statsmodels.api as sm
-
+import os
+import tkinter as tk
+from tkinter import filedialog, messagebox
+import sys
 
 df =
 
 
-# Perform logistic regression
+# Function to perform logistic regression
+
+def run_logsitic_regression(df):
+
+    df['version'] = df['version'].astype('category')
+    df['test_number'] = df['test_number'].astype('category')
+
+    print(df)
 
 
-df['version'] = df['version'].astype('category')
-df['test_number'] = df['test_number'].astype('category')
-
-print(df)
+    df['count_undesired_action'] = df['sample_size'] - df['count_desired_action']
 
 
-df['count_undesired_action'] = df['sample_size'] - df['count_desired_action']
+    y = df[['count_desired_action','count_undesired_action']]
+    X = pd.get_dummies(df[['test_number','version']], drop_first=True).astype(float)
+    X = sm.add_constant(X)
 
 
-y = df[['count_desired_action','count_undesired_action']]
-X = pd.get_dummies(df[['test_number','version']], drop_first=True).astype(float)
-X = sm.add_constant(X)
+    model = sm.GLM(
+        endog=y, 
+        exog=X, 
+        family=sm.families.Binomial()
+    )
+    results = model.fit()
+    
+    return results
 
-
-model = sm.GLM(
-    endog=y, 
-    exog=X, 
-    family=sm.families.Binomial()
-)
-results = model.fit()
-print(results.summary())
-
-
-coef = results.params['B']
