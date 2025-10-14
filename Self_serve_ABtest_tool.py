1f195fbb6d22662f73ff42427e55a4cf07ca7121
Alexander Drew <alexander.william.drew@gmail.com>
2025-10-14 15:34:17 +0100

    Read csv file instead of trying to pass filepath as df. Added human readable output to logistic regression.

--- Self_serve_ABtest_tool.py
+++ Self_serve_ABtest_tool.py
@@ -6,11 +6,10 @@ from tkinter import filedialog, messagebox
 def run_logsitic_regression(df):
     import pandas as pd
     import statsmodels.api as sm
+    import numpy as np
 
     df['version'] = df['version'].astype('category')
     df['test_number'] = df['test_number'].astype('category')
-
-    print(df)
 
 
     df['count_undesired_action'] = df['sample_size'] - df['count_desired_action']
@@ -27,7 +26,25 @@ def run_logsitic_regression(df):
         family=sm.families.Binomial()
     )
     results = model.fit()
-    
+    coef = results.params['version_B']
+    pval = results.pvalues['version_B']
+    odds_ratio = np.exp(coef)
+
+    if pval <= 0.05:
+        strength = "STRONG"
+    elif pval <= 0.1:
+        strength = "SUFFICIENT"
+    else:
+        strength = "INSUFFICIENT"
+        return strength
+
+    if odds_ratio >= 1:
+        print(f"People who saw version B were {odds_ratio:.2f}X as likely to perform the desired action as those who saw version A.")
+    else:
+        print(f"People who saw version B were {1/odds_ratio:.2f}X less likely to perform the desired action than those who saw version A.")
+
+    print(f"The p-value is: {pval:.4f}. Brand & Marketing consider this {strength} evidence that the difference is statistically significant.")
+
     return results
 
 
@@ -41,8 +58,10 @@ def select_file():
     )
 
     if file_path:
+        import pandas as pd
         # Call the processing function and pass the file path and campaign name
-        run_logsitic_regression(file_path)
+        retrieved_df = pd.read_csv(file_path)
+        run_logsitic_regression(retrieved_df)
         # Close the main window after processing is done
         root.destroy()
     else:
